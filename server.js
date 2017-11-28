b3d482aee786c61aed4df12d90b26690d0623c6e
Lex Myers <lex.myers@gmail.com>
2017-11-27 20:32:32 -0800

    fixes

--- server.js
+++ server.js
@@ -14,36 +14,44 @@ app.get('/', function(req, res) {
     if(!req.session.gold) {
         req.session.gold = 0;
         req.session.change = 0;
-    }
-    if(!res.session.record) {
+    };
+    if(!req.session.record) {
         req.session.record = [];
-    }
-    if()
+    };
     let info = {
         "gold": req.session.gold,
         "record": req.session.record,
-    }
+    };
+    console.log(info);
     res.render('index', info);
     res.end();
 });
 
-app.post('process_money', function(req, res) {
+app.post('/process_money', function(req, res) {
+    req.session.choice = req.body.choice;
     if(req.body.choice === 'farm') {
         change = Math.floor((Math.random() * 10) + 10);
-        req.session.gold += change;
-    } else if (req.body.choice === 'farm') {
+        req.session.gold += change;;
+    } else if (req.body.choice === 'cave') {
         change = Math.floor((Math.random() * 5) + 5);
         req.session.gold += change;    
-    } else if (req.body.choice === 'farm') {
+    } else if (req.body.choice === 'house') {
         change = Math.floor((Math.random() * 3) + 2);
         req.session.gold += change;
-    } else if (req.body.choice === 'farm') {
+    } else if (req.body.choice === 'casino') {
         change = Math.floor((Math.random() * 101) - 50);
         req.session.gold += change;
     };
     let newrec = {
         "choice": req.session.choice,
         "change": change,
-        "time": 
-    }
-})
+        "time": new Date()
+    };
+    console.log(newrec);
+    req.session.record.push(newrec);
+    res.redirect('/');
+});
+
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
+});
